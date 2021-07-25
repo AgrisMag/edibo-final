d301b38a7864aee44ffe356c20ac41bee6cb7695
Agris <agris.magdalenoks@gmail.com>
2021-07-25 21:20:31 +0300

    Initial commit

---
+++ controllers/auth.js
@@ -0,0 +1,44 @@
+const mysql = require("mysql2");
+const bcrypt = require("bcryptjs");
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+})
+
+exports.register = (req, res) => {
+    console.log(req.body);
+
+    const { name, email, password, passwordConfirm } = req.body;
+
+    db.query('SELECT email FROM users WHERE email =?', [email], async (error, results) => {
+        if (error) {
+            console.log(error)
+        }
+
+        if (results.length > 0) {
+            return res.render('register', {
+                message: 'That email is already taken!'
+            })
+        } else if (password !== passwordConfirm) {
+            return res.render('register', {
+                message: 'Passwords do not match!'
+            })
+        }
+
+        let hashedPassword = await bcrypt.hash(password, 10);
+        console.log(hashedPassword)
+
+        db.query('INSERT INTO users SET ?', { name: name, email: email, password: hashedPassword }, (error, results) => {
+            if (error) {
+                console.log(error);
+            } else {
+                return res.render('register', {
+                    message: 'User registered!'
+                })
+            }
+        })
+    });
+}
