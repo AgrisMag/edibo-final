272ab46ebc2e5afc7b754c68afc8565c53d15ef3
Agris <agris.magdalenoks@gmail.com>
2021-07-25 22:05:51 +0300

    setting up for tomorrow

--- controllers/auth.js
+++ controllers/auth.js
@@ -29,7 +29,39 @@ exports.register = (req, res) => {
         }
 
         let hashedPassword = await bcrypt.hash(password, 10);
-        console.log(hashedPassword)
+
+        db.query('INSERT INTO users SET ?', { name: name, email: email, password: hashedPassword }, (error, results) => {
+            if (error) {
+                console.log(error);
+            } else {
+                return res.render('register', {
+                    message: 'User registered!'
+                })
+            }
+        })
+    });
+}
+exports.login = (req, res) => {
+    console.log(req.body);
+
+    const { email, password } = req.body;
+
+    db.query('SELECT email FROM users WHERE email =?', [email], async (error, results) => {
+        if (error) {
+            console.log(error)
+        }
+
+        if (results.length > 0) {
+            return res.render('register', {
+                message: 'That email is already taken!'
+            })
+        } else if (password !== passwordConfirm) {
+            return res.render('register', {
+                message: 'Passwords do not match!'
+            })
+        }
+
+        let hashedPassword = await bcrypt.hash(password, 10);
 
         db.query('INSERT INTO users SET ?', { name: name, email: email, password: hashedPassword }, (error, results) => {
             if (error) {
