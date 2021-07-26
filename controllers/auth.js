03442cbee25117175dbaa225ff23f058a7f7da5d
Agris <agris.magdalenoks@gmail.com>
2021-07-26 16:26:47 +0300

    added login option

--- controllers/auth.js
+++ controllers/auth.js
@@ -41,36 +41,34 @@ exports.register = (req, res) => {
         })
     });
 }
-exports.login = (req, res) => {
-    console.log(req.body);
+
+exports.login = async (req, res) => {
 
     const { email, password } = req.body;
 
-    db.query('SELECT email FROM users WHERE email =?', [email], async (error, results) => {
+    db.query('SELECT * FROM users WHERE email = ?', [email], async function (error, results, fields) {
         if (error) {
-            console.log(error)
-        }
-
-        if (results.length > 0) {
-            return res.render('register', {
-                message: 'That email is already taken!'
+            res.send({
+                "code": 400,
+                "failed": "error ocurred"
             })
-        } else if (password !== passwordConfirm) {
-            return res.render('register', {
-                message: 'Passwords do not match!'
-            })
-        }
-
-        let hashedPassword = await bcrypt.hash(password, 10);
-
-        db.query('INSERT INTO users SET ?', { name: name, email: email, password: hashedPassword }, (error, results) => {
-            if (error) {
-                console.log(error);
-            } else {
-                return res.render('register', {
-                    message: 'User registered!'
+        } else {
+            if (results.length > 0) {
+                const comparison = await bcrypt.compare(password, results[0].password)
+                if (comparison) {
+                    res.render('profile')
+                }
+                else {
+                    return res.render('login', {
+                        message: 'Email and password does not match'
+                    })
+                }
+            }
+            else {
+                return res.render('login', {
+                    message: 'Email does not exist'
                 })
             }
-        })
+        }
     });
 }
